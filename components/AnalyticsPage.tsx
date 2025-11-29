357502da8de756ef9f86036b3d10aee1760a1907
zaiyaan najam <zaiyaannajam@Zaiyaans-MacBook-Air.local>
2025-11-29 02:09:05 -0500

    build fix

--- components/AnalyticsPage.tsx
+++ components/AnalyticsPage.tsx
@@ -202,7 +202,7 @@ export default function AnalyticsPage() {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                  label={({ name, percent }) => `${name}: ${((percent || 0) * 100).toFixed(0)}%`}
                   outerRadius={100}
                   fill="#8884d8"
                   dataKey="value"
